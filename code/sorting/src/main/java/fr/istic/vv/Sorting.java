85dad13e31237d29fa803ad2d08c0be53b7b6272
slocke <stefan.locke@etudiant.univ-rennes1.fr>
2022-03-15 11:10:56 +0100

    added sorting algo

--- code/sorting/src/main/java/fr/istic/vv/Sorting.java
+++ code/sorting/src/main/java/fr/istic/vv/Sorting.java
@@ -3,10 +3,31 @@ import java.util.Comparator;
 
 public class Sorting {
 
-    public static <T>  T[] bubblesort(T[] array, Comparator<T> comparator) { return null; }
+    public static <T>  T[] bubblesort(T[] array, Comparator<T> comparator) {
+
+        int swaps = 0;
+        do {
+            for (int i = 0; i < array.length-1; i++){
+                if (comparator.compare(array[i],array[i+1]) < 0) {
+                    swap(array,i,i+1);
+                    swaps++;
+                }
+            }
+        } while (swaps != 0);
+        return array; }
 
     public static <T> T[] quicksort(T[] array, Comparator<T> comparator)  { return null; }
 
+
+
+
     public static <T> T[] mergesort(T[] array, Comparator<T> comparator) { return null; }
 
+
+
+    private static <T> void swap(T[] array, int i, int j){
+        T temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
--- code/sorting/src/test/java/fr/istic/vv/SortingTest.java
+++ code/sorting/src/test/java/fr/istic/vv/SortingTest.java
@@ -1,9 +1,17 @@
 package fr.istic.vv;
 import net.jqwik.api.*;
 
+import java.util.Comparator;
+
 public class SortingTest {
     @Property
     boolean absoluteValueOfAllNumbersIsPositive(@ForAll int anInteger) {
         return Math.abs(anInteger) >= 0;
+    }
+
+
+
+    @Property
+    boolean testBubbleSort(){
     }
 }
